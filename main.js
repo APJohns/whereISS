e52dffffc507bf6df20148283d194ee37ac054f2
APJohns <ashleypjohns@gmail.com>
2019-02-20 21:39:33 -0500

    Add flyover times and some controls

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+
+// Map Scripts
+
 let circle = undefined;
+let autoPan = true;
 
 async function fetchAsync(url) {
     let response = await fetch(url);
@@ -17,10 +21,10 @@ async function getISSPos(map) {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
-        radius: 1000
+        radius: 100000
     }).addTo(map);
 
-    map.flyTo([lat, lon], 3);
+    if (autoPan) map.flyTo([lat, lon], 3);
 }
 
 async function startMap(map) {
@@ -44,3 +48,32 @@ startMap(mymap);
 let update = setInterval(() => {
     getISSPos(mymap);
 }, 10000);
+
+mymap.on('mousedown', () => {
+    autoPan = false;
+});
+
+
+// Interface Scripts
+
+const follow = document.getElementById('follow');
+follow.addEventListener('click', (e) => {
+    autoPan = true;
+    getISSPos(mymap);
+});
+
+const addressForm = document.getElementById('addressForm');
+const addressField = document.getElementById('addressField');
+addressForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    autoPan = false;
+    let address = addressField.value;
+    let data = await fetchAsync(`http://www.mapquestapi.com/geocoding/v1/address?key=NBFsYD4ZCez8frZzKXwGsmTOwhBw57NJ&location=${address}`);
+    let location = data.results[0].locations[0].latLng;
+    let marker = L.marker([location.lat, location.lng]).addTo(mymap);
+
+    let flyover = await fetchAsync(`https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-pass.json?lat=${location.lat}&lon=${location.lng}`);
+    let flyoverTimes = flyover.response;
+    marker.bindPopup(`<h2>Flyover Times</h2><p>${new Date(flyoverTimes[0].risetime * 1000)}</p>`).openPopup();
+    mymap.flyTo([location.lat, location.lng], 6);
+});
